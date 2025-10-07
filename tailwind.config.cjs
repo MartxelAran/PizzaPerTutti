8a0ffa2336002859da96e5e59aa4d5c7b2cfaf32
Martxel <56430705+MartxelAran@users.noreply.github.com>
2025-10-07 19:19:34 +0200

    First commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+theme: {
+extend: {
+colors: {
+'ppt-red': '#C0392B',
+'ppt-cream': '#F8E5C0',
+'ppt-wood': '#2C2C2C'
+},
+fontFamily: {
+title: ['Lobster', 'cursive'],
+body: ['Poppins', 'sans-serif']
+}
+}
+},
+plugins: []
+};
