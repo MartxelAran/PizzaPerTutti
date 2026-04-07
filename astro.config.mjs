44d05c83e10e767b8908c116f892e637f3a2fb1d
Martxel Aranzadi Olazabal <maranzadi@arfima.com>
2026-04-07 13:45:21 +0200

    Change url and base
    Signed-off-by: Martxel Aranzadi Olazabal <maranzadi@arfima.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://martxelaran.github.io',
-  base: '/PizzaPerTutti/',
+  site: 'https://pizzapertutti.net',
+  base: '/',
   vite: {
     plugins: [tailwindcss()]
   }
-});
+})
